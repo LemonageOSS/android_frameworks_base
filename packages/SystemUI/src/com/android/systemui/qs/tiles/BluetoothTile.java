2fd68ffa4a580f96b3cd18c484e45bf376367d6a
rickastlee <45075899+rickastlee@users.noreply.github.com>
2025-04-26 20:49:15 +0200

    Revert "SystemUI: Enable and fix QS detail view, adapt layout to 11"
    This reverts commit 8f6faf7913bcede50889254642597a611e58b251.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -250,7 +250,6 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
             refreshState();
             if (isShowingDetail()) {
                 mDetailAdapter.updateItems();
-                mDetailAdapter.setItemsVisible(true);
                 fireToggleStateChanged(mDetailAdapter.getToggleState());
             }
         }
